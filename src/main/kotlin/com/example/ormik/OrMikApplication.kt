70c9499807fcea16668e5f6d92df76fe1e0297e6
Ernest Staszuk <ernest.staszuk@eobuwie.com.pl>
2024-06-13 00:58:00 +0200

    01-greenfield

---
+++ src/main/kotlin/com/example/ormik/OrMikApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ormik
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrMikApplication
+
+fun main(args: Array<String>) {
+  runApplication<OrMikApplication>(*args)
+}
