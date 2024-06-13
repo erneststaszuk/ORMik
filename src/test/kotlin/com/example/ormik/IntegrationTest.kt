0e4e2f6f0da2355bb7ab25c669591fbf8033f626
Ernest Staszuk <ernest.staszuk@eobuwie.com.pl>
2024-06-13 04:32:17 +0200

    04-transactions

--- src/test/kotlin/com/example/ormik/IntegrationTest.kt
+++ src/test/kotlin/com/example/ormik/IntegrationTest.kt
@@ -1,5 +1,6 @@
 package com.example.ormik
 
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
@@ -7,4 +8,5 @@ import org.springframework.test.context.ActiveProfiles
 @ActiveProfiles("integration")
 @Import(TestcontainersConfiguration::class)
 @SpringBootTest
+@ExtendWith(ClearDatabaseExtension::class)
 interface IntegrationTest
